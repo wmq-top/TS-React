87bf5317c3dd58da7b4ea028fe35e993f39d879f
geeker <979110555@qq.com>
2022-05-13 11:07:11 +0800

    change button color

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ function App() {
 
   return (
     <div className="App">
-      <Button type="primary" text="确认" disabled />
-      <Button type="danger" text="危险" block />
+      <Button type="primary" text="确认" />
+      <Button type="danger" text="危险" />
       <Button type="warning" text="警告" />
       <Button type="cancel" text="取消" />
       <Button type="link" text="链接" />
